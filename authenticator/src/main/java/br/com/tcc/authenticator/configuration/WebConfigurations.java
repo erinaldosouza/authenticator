bd1dbb32da11b018a4147dd6f7a2b0d83fd2b53b
t_francisco <t_francisco@MATRIZ-300681.hapvida.com.br>
2019-06-11 16:35:12 -0300

    rest template with default timeout

--- authenticator/src/main/java/br/com/tcc/authenticator/configuration/WebConfigurations.java
+++ authenticator/src/main/java/br/com/tcc/authenticator/configuration/WebConfigurations.java
@@ -2,14 +2,26 @@ package br.com.tcc.authenticator.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class WebConfigurations {
-	
+
 	@Bean
 	protected RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		return new RestTemplate(getClientHttpRequestFactory());
 	}
 
+	private ClientHttpRequestFactory getClientHttpRequestFactory() {
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		
+		// Connect timeout
+		clientHttpRequestFactory.setConnectTimeout(5000);
+		
+		// Read timeout
+		clientHttpRequestFactory.setReadTimeout(0);
+		return clientHttpRequestFactory;
+	}
 }
